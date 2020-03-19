cbfb3a6bacd4e19e80d4f350a7e36b43538023e8
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-03-20 00:00:56 +0530

    Add files via upload

---
+++ minStepsAddOrSubtractToTarget.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define bug1 cout<<"hi1"<<endl
+#define bug2 cout<<"hi2"<<endl
+#define bug3 cout<<"hi3"<<endl
+const int N=1e5;
+const int mod=1e9+7;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int i,n;
+    cin>>n;
+
+    while(i*(i+1)/2<=n) i++;
+
+    cout<<i;
+
+    return 0;
+}
