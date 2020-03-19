1203fb3fe0fddb42155ab8381d56ddbd10868e4d
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-03-20 00:01:33 +0530

    Add files via upload

---
+++ minMails.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define pii pair<int,int>
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define bug1 cout<<"hi1"<<endl
+#define bug2 cout<<"hi2"<<endl
+#define bug3 cout<<"hi3"<<endl
+const int N=1e5;
+const int mod=1e9+7;
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	int n,k,x;
+	cin>>n>>k>>x;
+
+	cout<<(n-1) + ceil(((n-1)*(1.0))/x)*(n-1) + ceil((n*(1.0))/x)*(k-n);
+
+	return 0;
+}
